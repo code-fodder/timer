a7412e2a02131fb3ab1b0a320246c1e29ff7ae30
adadachanji@gmail.com <adadachanji@gmail.com>
2020-05-21 22:10:44 +0100

    Added gate tests

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include "sync.hpp"
 
+// Not sure what the true accuracy of the standard linux timer is. Usually it is less then 10ms
+// but I have seen it up to 20-25ms and once I saw it at 40ms!
+const int64_t acceptable_error_ms = 50;
+
 class timer_test
 {
 public:
@@ -30,9 +34,8 @@ private:
     thread_sync::stopwatch m_sw;
 };
 
-int main()
+bool test_timer()
 {
-    const int64_t acceptable_error_ms = 30;
     int64_t time_deviation_ms = 0;
     int64_t time_test_value = 0;
     timer_test test;
@@ -49,7 +52,7 @@ int main()
     if (std::abs(time_deviation_ms) > acceptable_error_ms)
     {
         std::cout << "\tFAILED: timer did not stop in time!\n";
-        return 1;
+        return false;
     }
     else
     {
@@ -67,7 +70,7 @@ int main()
     if (std::abs(time_deviation_ms) > acceptable_error_ms)
     {
         std::cout << "\tFAILED: timer did not stop in time!\n";
-        return 1;
+        return false;
     }
     else
     {
@@ -75,7 +78,7 @@ int main()
     }
 
     std::cout << "\nContinuous timer loss of precision test over a longer time";
-    time_test_value = 10000;
+    time_test_value = 5000;
     test.start(100, false);
     SLEEP_MSEC(time_test_value);
     test.stop();
@@ -85,11 +88,79 @@ int main()
     if (std::abs(time_deviation_ms) > acceptable_error_ms)
     {
         std::cout << "\tFAILED: timer lost too much time!\n";
-        return 1;
+        return false;
     }
     else
     {
         std::cout << "\tPASSED: timer kept time within acceptable time margine\n";
+    }
+
+    return true;
+}
+
+bool test_gate()
+{
+    thread_sync::gate test_gate;
+    thread_sync::stopwatch sw;
+    int64_t time_deviation_ms = 0;
+    int64_t time_test_value = 0;
+
+    std::cout << "\nTest gate opens after 100ms timeout\n";
+    time_test_value = 100;
+    sw.restart();
+    test_gate.wait_at_gate(static_cast<unsigned int>(time_test_value));
+    std::cout << "\tstopped at: " << sw.get_elapsed_time() << "ms" << std::endl;
+    time_deviation_ms = static_cast<int64_t>(sw.get_elapsed_time()) - time_test_value;
+    std::cout << "\tTime deviation: " << time_deviation_ms << "ms\n";
+    if (std::abs(time_deviation_ms) > acceptable_error_ms)
+    {
+        std::cout << "\tFAILED: gate did not open within acceptable timeout limit\n";
+        return false;
+    }
+    else
+    {
+        std::cout << "\tPASSED: timer kept time within acceptable time margine\n";
+    }
+
+    std::cout << "\nTest gate waiting for a long time is opened after 200ms by open_gate() from another thread\n";
+    time_test_value = 200;
+    sw.restart();
+    // Thread will open the gate after a delay
+    std::thread th([&test_gate, &sw, time_test_value]{
+        SLEEP_MSEC(time_test_value);
+        test_gate.open_gate();
+    });
+    // for 10 times longer the thread - let the thread open the gate
+    test_gate.wait_at_gate(static_cast<unsigned int>(time_test_value * 10));
+    std::cout << "\tstopped at: " << sw.get_elapsed_time() << "ms" << std::endl;
+    time_deviation_ms = static_cast<int64_t>(sw.get_elapsed_time()) - time_test_value;
+    std::cout << "\tTime deviation: " << time_deviation_ms << "ms\n";
+    if (std::abs(time_deviation_ms) > acceptable_error_ms)
+    {
+        std::cout << "\tFAILED: gate did not open within acceptable timeout limit\n";
+        return false;
+    }
+    else
+    {
+        std::cout << "\tPASSED: timer kept time within acceptable time margine\n";
+    }
+    thread_sync::threading::join_thread(th);
+
+    return true;
+}
+
+int main()
+{
+    // Run gate test since it users timer
+    if (!test_gate())
+    {
+        return 1;
+    }
+
+    // Run timer tests
+    if (!test_timer())
+    {
+        return 1;
     }
 
     std::cout << "\n==========================\n";
